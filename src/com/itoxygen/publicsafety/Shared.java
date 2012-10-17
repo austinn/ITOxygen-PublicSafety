dc26d9dc118b9fad2e6d2fb500eb5389223f7689
sathorpe <sathorpe@mtu.edu>
2012-10-17 15:02:37 -0400

    Add static shared class to open pdf for both activities.  Now opens files as pdfs.

---
+++ src/com/itoxygen/publicsafety/Shared.java
@@ -0,0 +1,17 @@
+package com.itoxygen.publicsafety;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+public class Shared {
+	public static void openPdf(File file, Activity activity) {
+		Uri targetUri = Uri.fromFile(file);
+		
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setDataAndType(targetUri, "application/pdf");
+		activity.startActivity(intent);
+	}
+}
